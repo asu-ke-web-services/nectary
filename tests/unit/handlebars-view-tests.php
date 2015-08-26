48c0d210a5f35e87b22a1418b1dca66e7fd7f4bb
Ivan Montiel <idmontie@asu.edu>
2015-08-26 13:29:47 -0700

    Add todo placeholder tests

--- tests/unit/handlebars-view-tests.php
+++ tests/unit/handlebars-view-tests.php
@@ -13,4 +13,28 @@ class Handlebars_View_Test extends \PHPUnit_Framework_TestCase {
   function test_exists() {
     $this->assertEquals( 'Nectary\Views\Handlebars_View', Views\Handlebars_View::class );
   }
+
+  function test_loads_in_files_from_different_view_roots() {
+    // TODO
+  }
+
+  function test_renders_basic_handlebars_templates() {
+    // TODO
+  }
+
+  function test_renders_conditionals_correctly() {
+    // TODO
+  }
+
+  function test_special_conditional_helper() {
+    // TODO
+  }
+
+  function test_special_html_special_chars_helper() {
+    // TODO
+  }
+
+  function test_add_strip_tags_helper() {
+    // TODO
+  }
 }
--- tests/unit/singleton-test.php
+++ tests/unit/singleton-test.php
@@ -13,4 +13,16 @@ class Singleton_Test extends \PHPUnit_Framework_TestCase {
   function test_exists() {
     $this->assertEquals( 'Nectary\Singleton', Singleton::class );
   }
+
+  function test_cannot_be_constructed() {
+    // TODO
+  }
+
+  function test_there_can_only_be_one() {
+    // TODO
+  }
+
+  function test_not_clonable() {
+    // TODO
+  }
 }
