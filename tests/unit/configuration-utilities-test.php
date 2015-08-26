8c77e25e2c8da0d4f8f7b682532566ce4d9d89fd
Ivan Montiel <idmontie@asu.edu>
2015-08-26 10:58:47 -0700

    Finish configuration utilities test

--- tests/unit/configuration-utilities-test.php
+++ tests/unit/configuration-utilities-test.php
@@ -1,18 +1,40 @@
 <?php
 
-namespace Nectary\Tests;
+namespace Nectary;
+
+use Nectary\Configuration;
 
 /**
- * Test the configuration class in the framework
+ * Test the configuration utilities in the framework
  *
  * @group configuration
  */
 class Configuration_Utilities_Test extends \PHPUnit_Framework_TestCase {
+  function setUp() {
+    Configuration::reset();
+  }
+
   function test_config_returns_corrent_data() {
-    // TODO
+    $mock = create_function_mock( $this, 'file_exists', 1 );
+    $mock->will( $this->returnValue( true ) );
+
+    $mock = create_function_mock( $this, 'file_get_contents', 1 );
+    $mock->will( $this->returnValue( 'key=1234' ) );
+
+    $value = config( 'key', 'default' );
+
+    $this->assertEquals( '1234', $value );
   }
 
   function test_config_returns_correct_default() {
+    $mock = create_function_mock( $this, 'file_exists', 1 );
+    $mock->will( $this->returnValue( true ) );
 
+    $mock = create_function_mock( $this, 'file_get_contents', 1 );
+    $mock->will( $this->returnValue( 'key=1234' ) );
+
+    $value = config( 'wrong_key', 'default' );
+
+    $this->assertEquals( 'default', $value );
   }
 }
