225f0f7e37d333ce92e57bce93ead0fa5fa26e9b
Ivan Montiel <idmontie@asu.edu>
2015-08-31 13:34:24 -0700

    Add tests for Json Feed and Generic JSON Facade

--- src/facades/implementations/generic-json-facade.php
+++ src/facades/implementations/generic-json-facade.php
@@ -6,20 +6,16 @@ use Nectary\Models\Json_Feed;
 use Nectary\Facades\Rss_Facade;
 
 class Generic_Json_Facade extends Rss_Facade {
-  private $url;
-
   public function load_dependencies() {
     return array( $this, 'get_feed' );
   }
 
   public function get_feed( $url ) {
-    $this->url = $url;
-
     return new Json_Feed( $url, array( $this, 'get_curl_feed_data' ) );
   }
 
-  public function get_curl_feed_data() {
-    $session = curl_init( $this->url );
+  public function get_curl_feed_data( $url ) {
+    $session = curl_init( $url );
 
     curl_setopt( $session, CURLOPT_RETURNTRANSFER, true );
 
