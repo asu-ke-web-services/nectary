f25c801f89f3864f099171e2b99ac784c105052f
Ivan Montiel <idmontie@asu.edu>
2015-09-01 13:57:10 -0700

    Finish html carousel factory and add tests

--- src/factories/implementations/html-carousel-factory.php
+++ src/factories/implementations/html-carousel-factory.php
@@ -10,10 +10,10 @@ class Html_Carousel_Factory extends Html_Factory {
   private $id;
 
   public function __construct() {
-    $this->id         = 'TODO';
-    $this->indivators = '';
-    $this->slides     = [];
-    $this->classes    = '';
+    $this->id            = uniqid( 'carousel-' );
+    $this->indicators    = false;
+    $this->slides        = [];
+    $this->classes       = '';
     $this->inner_classes = '';
   }
 
@@ -22,31 +22,51 @@ class Html_Carousel_Factory extends Html_Factory {
   }
 
   public function add_class( $classes ) {
-    $this->classes .= $classes;
+    $this->classes .= ' ' . $classes;
   }
 
   public function add_inner_class( $classes ) {
-    $this->inner_classes .= $classes;
+    $this->inner_classes .= ' ' . $classes;
   }
 
-
   public function turn_on_indicators() {
-    // TODO
-      // <ol class="carousel-indicators">
-      //   <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-      //   <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      //   <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-      // </ol>
+    $this->indicators = true;
   }
 
   public function build() {
     $html = join( $this->slides, '' );
+
+    if ( $this->indicators ) {
+      $indicators = $this->build_indicators();
+    } else {
+      $indicators = '';
+    }
+
     return 
     "<div id='{$this->id}' class='carousel slide {$this->classes}' data-ride='carousel'>
       {$this->indicators}
       <div class='carousel-inner {$this->inner_classes}' role='listbox'>
         {$html}
+        {$indicators}
       </div>
     </div>";
+  }
+
+  private function build_indicators() {
+    $number = count( $this->slides );
+    $inner_indicators = '';
+
+    for ( $i = 0; $i < $number; $i++ ) {
+      $classes = ( $i === 0 ) ? 'active' : '';
+      $inner_indicators .= "
+        <li data-target='#{$this->id}' data-slide-to='{$i}' class='{$classes}'></li>
+      ";
+    }
+
+    return "
+      <ol class='carousel-indicators'>
+        {$inner_indicators}
+      </ol>
+    ";
   }
 }
