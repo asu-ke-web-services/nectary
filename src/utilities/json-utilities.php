1fcfe93183ea924fba095c93777eadaa1f9579f7
Ivan Montiel <idmontie@asu.edu>
2015-09-01 13:05:33 -0700

    Add and fix functionality for json utilities

--- src/utilities/json-utilities.php
+++ src/utilities/json-utilities.php
@@ -6,6 +6,16 @@ namespace Nectary\Utilities;
  * Helper utility functions for dealing with JSON
  */
 class Json_Utilities {
+  /**
+   * Provide a path to the given json array:
+   * 
+   * ```
+   * self::get( $json, 'key1.key2.key3' );
+   * ```
+   * 
+   * This will return the value of however far it was
+   * able to go
+   */
   public static function get( $json, $path ) {
     $path_parts = explode( '.', $path );
 
@@ -16,27 +26,20 @@ class Json_Utilities {
            array_key_exists( $part, $current ) ) {
         $current = $current[ $part ];  
       } else {
-        break;
+        return null;
       }
     }
 
     return $current;
   }
 
-  public static function get_or_default( $json, $path, $default ) {
-    $path_parts = explode( '.', $path );
-
-    $current = $json;
-
-    foreach ( $path_parts as $part ) {
-      if ( is_array( $current ) &&
-           array_key_exists( $part, $current ) ) {
-        $current = $current[ $part ];  
-      } else {
-        return $default;
-      }
+  public static function get_or_default( $json, $path, $default = null ) {
+    $ref = self::get( $json, $path );
+    
+    if ( $ref === null ) {
+      return $default;
+    } else {
+      return $ref;
     }
-
-    return $current;
   }
 }
--- tests/unit/json-utilities-test.php
+++ tests/unit/json-utilities-test.php
@@ -41,7 +41,7 @@ class Json_Utilities_Test extends \PHPUnit_Framework_TestCase {
     $this->assertEquals( 'value', $value );
   }
 
-  function test_will_quietly_fail_while_walking() {
+  function test_will_return_null_on_fail() {
     $raw = "
       {
         \"key1\" : {
@@ -55,7 +55,24 @@ class Json_Utilities_Test extends \PHPUnit_Framework_TestCase {
     $json = json_decode( $raw, true );
 
     $value = Json_Utilities::get( $json, 'key1.key2.keyNope' );
-    $this->assertEquals( [ 'key3' => 'value' ], $value );
+    $this->assertEquals( null, $value );
+  }
+
+  function test_will_return_null_value() {
+    $raw = "
+      {
+        \"key1\" : {
+          \"key2\" : {
+            \"key3\" : \"null\"
+          }
+        }
+      }
+    ";
+
+    $json = json_decode( $raw, true );
+
+    $value = Json_Utilities::get( $json, 'key1.key2.key3' );
+    $this->assertEquals( 'null', $value );
   }
 
   function test_will_return_real_value_when_providing_a_default() {
@@ -75,7 +92,7 @@ class Json_Utilities_Test extends \PHPUnit_Framework_TestCase {
     $this->assertEquals( 'value', $value );
   }
 
-  function test_will_provide_default_if_fail() {
+  function test_will_provide_default_when_key_does_not_exist() {
     $raw = "
       {
         \"key1\" : {
@@ -90,5 +107,65 @@ class Json_Utilities_Test extends \PHPUnit_Framework_TestCase {
 
     $value = Json_Utilities::get_or_default( $json, 'key1.key2.keyNope', '???' );
     $this->assertEquals( '???', $value );
+  }
+
+  function test_will_access_arrays() {
+    $raw = "
+      {
+        \"key1\" : {
+          \"key2\" : {
+            \"key3\" : [
+              \"value1\",
+              \"value2\"
+            ]
+          }
+        }
+      }
+    ";
+
+    $json = json_decode( $raw, true );
+
+    $value = Json_Utilities::get( $json, 'key1.key2.key3.1' );
+    $this->assertEquals( 'value2', $value );
+  }
+
+  function test_will_return_null_when_accessing_non_existant_array_index() {
+    $raw = "
+      {
+        \"key1\" : {
+          \"key2\" : {
+            \"key3\" : [
+              \"value1\",
+              \"value2\"
+            ]
+          }
+        }
+      }
+    ";
+
+    $json = json_decode( $raw, true );
+
+    $value = Json_Utilities::get( $json, 'key1.key2.key3.100' );
+    $this->assertEquals( null, $value );
+  }
+
+  function test_will_return_array() {
+    $raw = "
+      {
+        \"key1\" : {
+          \"key2\" : {
+            \"key3\" : [
+              \"value1\",
+              \"value2\"
+            ]
+          }
+        }
+      }
+    ";
+
+    $json = json_decode( $raw, true );
+
+    $value = Json_Utilities::get( $json, 'key1.key2.key3' );
+    $this->assertEquals( array( 'value1', 'value2' ), $value );
   }
 }
