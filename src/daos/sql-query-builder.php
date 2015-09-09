dd357f82facd16470a610c2e0682328722e6c6ee
Ivan Montiel <idmontie@asu.edu>
2015-09-09 13:52:40 -0700

    Add Dao and sql builder

---
+++ src/daos/sql-query-builder.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Nectary\Daos;
+
+/**
+ * Select_SQL_Query_Builder - Helper class for building PDO select statements
+ *
+ * @author Ryan Raub
+ */
+class Select_SQL_Query_Builder {
+
+  /**
+   * Internal state for this object
+   */
+  private $columns_to_select, $from, $joins, $where_clause, $values_to_bind, $order_by, $limit;
+
+  /**
+   * Sets up default state
+   */
+  public function __construct() {
+    $this->columns_to_select = array();
+    $this->from              = 'TABLE_NAME';
+    $this->joins             = '';
+    $this->where_clause      = ' 1=1 ';
+    $this->values_to_bind    = array();
+    $this->group_by          = array();
+    $this->order_by          = '';
+    $this->limit             = 100;
+  }
+
+  /**
+   * This function takes either a string or an one dimensional array.
+   * the string can be a single column name or multiple columns comma seperated.
+   * @param $new_columns : string or array of column names to select
+   */
+  public function add_columns( $new_columns ) {
+    $this->columns_to_select = array_merge( $this->columns_to_select, to_array( $new_columns ) );
+  }
+
+  /**
+   * Mainly for debugging, probably wont need this
+   * @return the current colums this query will select from
+   */
+  public function get_columns() {
+    return $this->columns_to_select;
+  }
+
+  /**
+   * For setting what table to select from, can only be one table. For joins see joins()
+   * @param $table : string - name of table to select
+   */
+  public function from( $table ) {
+    $this->from = $table;
+  }
+
+  /**
+   * For adding a join, can be called multiple times for multiple joins.
+   * @param $join_statement : string - sql fragment of the join statement
+   */
+  public function joins( $join_statement ) {
+    $this->joins .= $join_statement;
+  }
+
+  /**
+   * For setting a limit on the query other than the default
+   * @param $new_limit : integer
+   */
+  public function limit( $new_limit ) {
+    $this->limit = intval( $new_limit );
+  }
+
+  /**
+   * For setting an order on the query other than the default. Currently Can't have multiple.
+   * @param $order : string : eg: "first_name ASC"
+   */
+  public function order_by( $order ) {
+    $this->order_by = $order;
+  }
+
+  /**
+   * For setting an group by on the query other than the default.
+   * @param $order : string : eg: "first_name"
+   */
+  public function group_by( $group ) {
+    $this->group_by[] = $group;
+  }
+
+  /**
+   * Adds a string to the where clause, it must start with an 'AND' or 'OR' to chain together
+   * criteria.
+   * @param $string : eg: "AND blah = foo"
+   */
+  public function where( $string ) {
+    $this->where_clause .= ' ' . $string . ' ';
+  }
+
+  /**
+   * Adds a string to the where clause with an 'AND'
+   * @param $string : eg: "blah = foo"
+   */
+  public function and_where( $string ) {
+    $this->where( 'AND ' . $string );
+  }
+
+  /**
+   * Adds a string to the where clause with an 'AND'
+   * @param $string : eg: "blah = foo"
+   */
+  public function or_where( $string ) {
+    $this->where( 'OR ' . $string );
+  }
+
+  /**
+   * Adds a variable to be bound to in the prepared statement
+   * @param $name : string
+   * @param $value : object
+   */
+  public function bind_value( $name, $value ) {
+    $this->values_to_bind[ $name ] = $value;
+  }
+
+  /**
+   * Uses the current state of the object to build the sql statement
+   * @return the sql select statement as a string
+   */
+  public function get_sql() {
+    if ( empty( $this->columns_to_select ) ) {
+      // this should be the default behavor
+      $this->columns_to_select = [ '*' ];
+    }
+
+    $from   = ' FROM ' . $this->from . ' ';
+    $from  .= $this->joins;
+    $from  .= ' WHERE ' . $this->where_clause;
+    $select = 'SELECT ' . implode( ', ', $this->columns_to_select ) . $from;
+
+    if ( ! empty( $this->group_by ) ) {
+      $select .= ' GROUP BY '.implode( ', ', $this->group_by ).' ';
+    }
+
+    if ( $this->order_by !== '' ) {
+      $select .= ' ORDER BY ' . $this->order_by . ' ';
+    }
+
+    $select .= ' LIMIT ' . $this->limit;
+    return $select;
+  }
+
+  /**
+   * Calls get_sql() to generate the sql and then creates the prepared statement
+   * and binds its values.
+   * @param $db : a pdo database connection
+   * @return pdo::Statement object with the query and values bound
+   */
+  public function get_statement( $db ) {
+    $statement = $db->prepare( $this->get_sql() );
+    foreach ( $this->values_to_bind as $name => $value ) {
+      $statement->bindValue( $name, $value );
+    }
+    return $statement;
+  }
+}
---
+++ tests/unit/sql-query-builder-test.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Nectary\Tests;
+
+use Nectary\Daos\Select_SQL_Query_Builder;
+
+/**
+ * SQL Query Builder Test
+ *
+ * @group daos
+ */
+class Sql_Query_Builder_Test extends \PHPUnit_Framework_TestCase {
+  public function test_constructor() {
+    $builder = new Select_SQL_Query_Builder();
+    $this->assertInstanceOf( 'Nectary\Daos\Select_SQL_Query_Builder', $builder );
+  }
+
+  public function test_that_a_simple_query_is_executeable() {
+    $builder = new Select_SQL_Query_Builder();
+    $builder->add_columns( 'people.person_id' );
+    $builder->from( 'people' );
+    $statement = $builder->get_sql();
+
+    $statement = preg_replace( '/\s+/', ' ', $statement );
+
+    $this->assertContains( 'SELECT people.person_id FROM people WHERE 1=1 LIMIT 100', $statement );
+  }
+
+  public function test_that_a_bare_query_is_executeable() {
+    $builder = new Select_SQL_Query_Builder();
+    $builder->from( 'people' );
+    $statement = $builder->get_sql();
+
+    $statement = preg_replace( '/\s+/', ' ', $statement );
+
+    $this->assertEquals( 'SELECT * FROM people WHERE 1=1 LIMIT 100', $statement );
+  }
+
+  public function test_where_clauses_incorrectly() {
+    // this should fail because we are using the where clause incorrectly, we are giving it a statement not a clause
+    $builder = new Select_SQL_Query_Builder();
+    $builder->from( 'people' );
+    $builder->where( 'AND 1<>1' );
+    $statement = $builder->get_sql();
+
+    $statement = preg_replace( '/\s+/', ' ', $statement );
+
+    $this->assertEquals( 'SELECT * FROM people WHERE 1=1 AND 1<>1 LIMIT 100', $statement );
+  }
+
+  public function test_and_where_clauses_correctly() {
+    // this should work because we are passing a statement to the and_where for it to build the clause
+    $builder = new Select_SQL_Query_Builder();
+    $builder->from( 'people' );
+    $builder->and_where( '1=1' );
+    $statement = $builder->get_sql();
+
+    $statement = preg_replace( '/\s+/', ' ', $statement );
+
+    $this->assertEquals( 'SELECT * FROM people WHERE 1=1 AND 1=1 LIMIT 100', $statement );
+  }
+
+  public function test_or_where_clauses_correctly() {
+    // this should work because we are passing a statement to the or_where for it to build the clause
+    $builder = new Select_SQL_Query_Builder();
+    $builder->from( 'people' );
+    $builder->or_where( '1=1' );
+    $statement = $builder->get_sql();
+
+    $statement = preg_replace( '/\s+/', ' ', $statement );
+
+    $this->assertEquals( 'SELECT * FROM people WHERE 1=1 OR 1=1 LIMIT 100', $statement );
+  }
+
+  public function test_add_columns_can_take_multiple_types_and_maintain_array() {
+    $builder = new Select_SQL_Query_Builder();
+    $builder->add_columns( 'people.person_id' );
+    $builder->add_columns( array( 'people.first_name' ) );
+    $builder->add_columns( 'people.last_name, people.middle_name' );
+    $builder->from( 'people' );
+    $statement = $builder->get_sql();
+
+    $statement = preg_replace( '/\s+/', ' ', $statement );
+
+    $this->assertEquals( 'SELECT people.person_id, people.first_name, people.last_name, people.middle_name FROM people WHERE 1=1 LIMIT 100', $statement );
+  }
+
+  public function test_that_a_more_complicated_query_is_executeable() {
+    $builder = new Select_SQL_Query_Builder();
+    $builder->add_columns( 'people.*' );
+    $builder->from( 'people' );
+    $builder->limit( 1 );
+    $builder->order_by( 'people.person_id DESC' );
+    $builder->joins( 'LEFT JOIN groups_people ON (groups_people.person_id = people.person_id)' );
+    $builder->add_columns( 'groups_people.*' );
+    $builder->where( 'AND people.person_id <> 1' );
+    $statement = $builder->get_sql();
+
+    $statement = preg_replace( '/\s+/', ' ', $statement );
+
+    $this->assertEquals( 'SELECT people.*, groups_people.* FROM people LEFT JOIN groups_people ON (groups_people.person_id = people.person_id) WHERE 1=1 AND people.person_id <> 1 ORDER BY people.person_id DESC LIMIT 1', $statement );
+  }
+
+  public function test_group_by() {
+    $builder = new Select_SQL_Query_Builder();
+    $builder->group_by( 'column-to-group-by' );
+    $sql = $builder->get_sql();
+
+    $sql = preg_replace( '/\s+/', ' ', $sql );
+
+    $this->assertContains( ' GROUP BY column-to-group-by ', $sql );
+  }
+
+  public function test_group_by_multiple() {
+    $builder = new Select_SQL_Query_Builder();
+    $builder->group_by( 'first-column-to-group-by' );
+    $builder->group_by( 'second-column-to-group-by' );
+    $sql = $builder->get_sql();
+
+    $sql = preg_replace( '/\s+/', ' ', $sql );
+
+    $this->assertContains( ' GROUP BY first-column-to-group-by, second-column-to-group-by', $sql );
+  }
+}
