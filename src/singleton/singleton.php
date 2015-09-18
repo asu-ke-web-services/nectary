70ab020d94875f5cdcd4d3b95ce600bd420ef641
Ivan Montiel <idmontie@asu.edu>
2015-09-18 16:44:12 -0700

    Remove wakeup exception until further notice

--- src/singleton/singleton.php
+++ src/singleton/singleton.php
@@ -6,9 +6,6 @@ abstract class Singleton {
   protected static $instances = array();
   protected function __construct() {}
   protected function __clone() {}
-  public function __wakeup() {
-    throw new \Exception( 'Cannot unserialize singleton' );
-  }
 
   public static function get_instance() {
     $cls = get_called_class(); // late-static-bound class name
